ba478a009ff19b8fb5c8a88826d55078606440ce
Zack-Area <zackarea10801087@gmail.com>
2023-02-27 06:32:04 -0800

    Task_7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string.
+ * @str: input string.
+ * Return: no return.
+ */
+void puts_half(char *str)
+{
+	int k = 0, i;
+
+	while (k >= 0)
+	{
+		if (str[k] == '\0')
+			break;
+		k++;
+	}
+
+	if (k % 2 == 1)
+		i = k / 2;
+	else
+		i = (k - 1) / 2;
+
+	for (i++; i < k; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
+
