97b91c712f2d021b3cafbc4086aea675208ff20c
Mr_Zack <61769917+Zack-Area@users.noreply.github.com>
2023-04-04 18:46:11 +0000

    Task_10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -33,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		prev = prev->next;
 	}
 
-	prev = prev->next;
+	curr = prev->next;
 	prev->next = curr->next;
 	free(curr);
 	return (1);
