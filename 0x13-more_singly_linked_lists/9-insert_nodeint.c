81ebd0990746013ff02655b904e5bea0b691f556
Mr_Zack <61769917+Zack-Area@users.noreply.github.com>
2023-04-04 17:58:33 +0000

    Task_9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,10 +10,24 @@
  * Return: Pointer to the new node, or NULL on failure.
  */
 
-listint_t *insert_nodeint_at_idx(listint_t **head, unsigned int idx, int n)
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *new_node, *current_node = *head;
 	unsigned int i;
+	listint_t *new_node;
+	listint_t *current_node;
+
+	current_node = *head;
+
+	if (idx != 0)
+	{
+		for (i = 0; i < idx - 1 && h != NULL; i++)
+		{
+			current_node = current_node->next;
+		}
+	}
+
+	if (current_node == NULL && idx != 0)
+		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -25,21 +39,13 @@ listint_t *insert_nodeint_at_idx(listint_t **head, unsigned int idx, int n)
 	{
 		new_node->next = *head;
 		*head = new_node;
-		return (new_node);
 	}
-
-	for (i = 0; i < (idx - 1); i++)
+	else
 	{
-		if (current_node == NULL || current_node->next == NULL)
-			return (NULL);
-
-		current_node = current_node->next;
+		new_node->next = current_node->next;
+		current_node->next = new;
 	}
-
-	new_node->next = current_node->next;
-	current_node->next = new_node;
 
 	return (new_node);
-
 }
 
