5e00d770ee18f18e7e18cfa602526a7bf87d7453
Mr_Zack <61769917+Zack-Area@users.noreply.github.com>
2023-04-08 03:39:18 +0000

    Task_2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -8,10 +8,10 @@
 *Return: The converted value
 */
 
-int get_bit(unsigned long int *n, unsigned int index)
+int get_bit(unsigned long int n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
+	if (index > 63)
 		return (-1);
-	
-	return (n >> index) & 1;
+
+	return ((n >> index) & 1);
 }
