6600262619d0ad8ff1cdbdf3cac2134106a001cb
Mr_Zack <61769917+Zack-Area@users.noreply.github.com>
2023-04-08 02:43:26 +0000

    commit

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,8 +2,8 @@
 #define MAIN_H
 
 unsigned int binary_to_uint(const char *b);
-int _putchar(char c);
 void print_binary(unsigned long int n);
+int _putchar(char c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
