f006cad792b6742a6b4c6ca11ad3f3100b3475b6
Mr_Zack <61769917+Zack-Area@users.noreply.github.com>
2023-04-08 02:51:18 +0000

    Task_0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,17 +9,18 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	int i;
 	unsigned int uint = 0;
-	int i = 0;
 
 	if (!b)
 		return (0);
 
-	while ((b[i] == '1') || (b[i] == '0'))
+	for (i = 0; b[i]; i++)
 	{
-		uint <<= 1;
-		uint += b[i] - '0';
-		i++;
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		uint = 2 * uint + (b[i] - '0');
 	}
+
 	return (uint);
 }
